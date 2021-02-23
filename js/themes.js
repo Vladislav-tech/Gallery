00a4757079b84fe1f4c76cf76c3961980c99165d
Vladislav Tseplyaev <vladislav.frint@gmail.com>
2021-02-23 11:11:52 +0300

    add footer

--- js/themes.js
+++ js/themes.js
@@ -1,12 +1,19 @@
 const btn = document.querySelector('.theme-icon-wrapper');
+const link = document.getElementById('theme');
+
+if (localStorage.getItem('theme') === "light") {
+    link.setAttribute('href', 'css/light.css');
+} else if (localStorage.getItem('theme') === 'dark') {
+    link.setAttribute('href', 'css/dark.css');
+}
 
 function changeTheme() {
-    const link = document.getElementById('theme');
-
     if (link.getAttribute('href') === 'css/dark.css') {
         link.setAttribute('href', 'css/light.css');
+        localStorage.setItem('theme', 'light');
     }   else {
         link.setAttribute('href', 'css/dark.css');
+        localStorage.setItem('theme', 'dark');
     }
 }
 
