d0d1ab974f17fad9140e6f486a8ad08c92e9886d
Vladislav Tseplyaev <vladislav.frint@gmail.com>
2021-02-28 12:53:08 +0300

    fixs: bug in #source/js/accordion.js

--- #source/js/accordion.js
+++ #source/js/accordion.js
@@ -1,15 +1,12 @@
-let galleryItems = document.querySelectorAll('.gallery-item');
+const galleryItems = document.querySelector('.gallery-items');
 
-for (const galleryItem of galleryItems) {
-    galleryItem.addEventListener('click', function(evt) {
-        if (evt.target.className !== 'gallery-item__share-image' && evt.target.className !== 'gallery-item__likes') {
-            console.log('true');
-            let shortDescription = galleryItem.lastElementChild;
-            if (shortDescription.style.maxHeight) {
-                shortDescription.style.maxHeight = null;
-            } else {
-                shortDescription.style.maxHeight = shortDescription.scrollHeight + "px";
-            }
+galleryItems.addEventListener('click', function(evt){
+    if (evt.target.className === 'gallery-item') {
+        let shortDescription = evt.target.lastElementChild;
+        if (shortDescription.style.maxHeight) {
+            shortDescription.style.maxHeight = null;
+        }   else {
+            shortDescription.style.maxHeight = shortDescription.scrollHeight + 'px';
         }
-    });
-}
+    } 
+});
